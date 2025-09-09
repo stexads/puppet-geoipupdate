7669fa02c05dbcc1d8a8f5d1f9f568957d055316
Stefano Longo <stefano@exads.com>
2025-09-09 11:13:23 +0100

    Added support for GeoIP.conf configuration file template

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
+#
 # Class geoipupdate
 #
-#
-
 
 class geoipupdate (
   String		$account_id,
@@ -9,4 +8,13 @@ class geoipupdate (
   Array[String]	$edition_ids,
 ) {
   notify { "======= Welcome to puppet-geoipupdate": }
+
+  notify { "The environment is ${$environment}": }
+  notify { "The module_name is ${module_name}": }
+
+  file { '/etc/GeoIP.conf':
+    ensure  => file,
+    content => template("${module_name}/GeoIP.conf.erb"),
+  }
+
 }
