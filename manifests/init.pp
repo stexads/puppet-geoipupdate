1964ce4efbdd7e35bee36229f61d01acb3a1f9be
Stefano Longo <stefano@exads.com>
2025-09-22 17:35:01 +0100

    puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,24 @@
 #
-# @summary Public class for installing, configuring and managing MaxMind's 'geoipupdate' client
+# @summary Main class for installing, configuring and managing MaxMind's 'geoipupdate' client
 #
-# @package_name
-# @presence_status
-# @conf_dir
-# @target_dir Note: module does not create the dir or test the existance or remove on module un-installation
-# @account_id
-# @license_key
-# @edition_ids
-# @timer_oncalendar
-#
-# @api public
-#
+# @param presence_status Installation value to be either: 'present' or 'absent'
+# @param target_dir Note: module does not create the dir or test the existance or remove on module un-installation
+# @param account_id Your MaxMind account ID
+# @param license_key Your MaxMind License Key
+# @param edition_ids Your MaxMind files to download
+# @param timer_oncalendar Defines timer triggerring of geoipupdate client calls
+# @param package_name Defaults to 'geoipupdate', which should not need to be overriden
+# @param conf_dir Directory where the configuration file `GeoIP.conf` will be written
 class geoipupdate (
-  String                      $package_name     = 'geoipupdate',
-  Enum[ "present", "absent" ] $presence_status,
-  String                      $conf_dir         = '/etc',
-  String                      $target_dir,
-  String                      $account_id,
-  String                      $license_key,
-  String                      $edition_ids,
-  String                      $timer_oncalendar,
+  Enum['present', 'absent'] $presence_status,
+  String                    $target_dir,
+  String                    $account_id,
+  String                    $license_key,
+  String                    $edition_ids,
+  String                    $timer_oncalendar,
+  String                    $package_name     = 'geoipupdate',
+  String                    $conf_dir         = '/etc',
 ) {
-
   package { 'geoipupdate':
     ensure => $presence_status,
     name   => $package_name,
@@ -46,8 +42,7 @@ class geoipupdate (
     ensure          => $presence_status,
     enable          => $_status,
     active          => $_status,
-    timer_content   => template("${module_name}/geoipupdate-update.timer.erb"),
-    service_content => template("${module_name}/geoipupdate-update.service.erb"),
+    timer_content   => template( "${module_name}/geoipupdate-update.timer.erb" ),
+    service_content => template( "${module_name}/geoipupdate-update.service.erb" ),
   }
-
 }
