e6e468abe7558f7488ac5d5af7d940b9997db449
Stefano Longo <stefano@exads.com>
2025-09-25 15:24:54 +0100

    Added support for running the geoipupdate client as a specified UNIX user

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,9 @@
 # @param account_id Your MaxMind account ID
 # @param license_key Your MaxMind License Key
 # @param edition_ids Your MaxMind files to download
-# @param timer_oncalendar Defines timer triggerring of geoipupdate client calls
+# @param user Set the UNIX user that the geoipupdate client process is executed as
 # @param package_name Defaults to 'geoipupdate', which should not need to be overriden
+# @param timer_oncalendar Defines timer triggerring of geoipupdate client calls
 # @param conf_dir Directory where the configuration file `GeoIP.conf` will be written
 # @param preserve_timestamps Whether to preserve modification times of files downloaded from the server
 # @param verbose Enable/disable verbose mode
@@ -18,6 +19,7 @@ class geoipupdate (
   String                    $account_id,
   String                    $license_key,
   String                    $edition_ids,
+  String                    $user                = 'root',
   String                    $package_name        = 'geoipupdate',
   String                    $timer_oncalendar    = 'daily',
   String                    $conf_dir            = '/etc',
